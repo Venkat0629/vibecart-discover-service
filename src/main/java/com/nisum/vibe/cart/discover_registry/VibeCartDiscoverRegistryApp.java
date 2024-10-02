e317be889b053da6eb2256770ed1e995c0841ae0
Venkat <venkatsaimane@gmail.com>
2024-10-02 17:52:37 +0530

    Updated project setup

--- src/main/java/com/nisum/vibe/cart/discover_registry/VibeCartDiscoverRegistryApp.java
+++ src/main/java/com/nisum/vibe/cart/discover_registry/VibeCartDiscoverRegistryApp.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoverRegistryApplication {
+public class VibeCartDiscoverRegistryApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoverRegistryApplication.class, args);
+		SpringApplication.run(VibeCartDiscoverRegistryApp.class, args);
 	}
 }
